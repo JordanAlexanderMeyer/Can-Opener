02c34f7247245108dc7237eeea2dbde6d614e0ca
Jordan Alexander Meyer <mac@eduroam-128-61-98-40.eduroam.gatech.edu>
2018-10-20 11:47:27 -0400

    added ssl redirect and removed ssl-express-www package

--- index.js
+++ index.js
@@ -137,11 +137,15 @@ var root = {
 
 var app = express();
 var port = process.env.PORT || 8080;
-var secure = require('ssl-express-www');
-
-app.use(secure);
 
 app.use(express.static(__dirname));
+
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  else
+    next()
+});
 
 app.get('/', function(req, res) {
   res.render('index.html');
